e74923f86cb4afc4c7a4bc92bb22229a2a5918c5
Alec Shiya <alec.shiya@gmail.com>
2018-01-17 19:01:26 +1300

    Added integration test framework so we can make sure we don't break stuff :)

---
+++ IntegrationTests/InMemoryServer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace IntegrationTests
+{
+    class InMemoryServer : IDisposable
+    {
+        private string _url = "http://www.in-memory-server.com";
+
+        private HttpServer _server;
+        private HttpClient _client;
+
+        public InMemoryServer()
+        {
+            var config = new HttpConfiguration();
+            refactor_me.WebApiConfig.Register(config);
+
+            _server = new HttpServer(config);
+            _client = new HttpClient(_server);
+        }
+
+        internal async Task<T> SendRequest<T>(string url, HttpMethod method)
+        {
+            var response = _client.SendAsync(createRequest(url, method));
+            return await response.Result.Content.ReadAsAsync<T>();
+        }
+
+        private HttpRequestMessage createRequest(string url, HttpMethod method)
+        {
+            var request = new HttpRequestMessage();
+
+            request.RequestUri = new Uri(_url + url);
+            request.Method = method;
+
+            return request;
+        }
+
+        private HttpRequestMessage createRequest<T>(string url, HttpMethod method, T content, MediaTypeFormatter formatter) where T : class
+        {
+            HttpRequestMessage request = createRequest(url, method);
+            request.Content = new ObjectContent<T>(content, formatter);
+
+            return request;
+        }
+
+        public void Dispose()
+        {
+            if (_server != null)
+            {
+                _server.Dispose();
+            }
+        }
+    }
+}
---
+++ IntegrationTests/ProductTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using refactor_me.Models;
+
+namespace IntegrationTests
+{
+    [TestClass]
+    [DeploymentItem("TestData", "TestData")]
+    public class ProductTests
+    {
+        [TestInitialize]
+        public void Init()
+        {
+            // Set directory for the test DB
+            Helpers.DataDirectory = System.IO.Path.GetFullPath(".\\TestData");
+        }
+
+        [TestMethod]
+        public async Task GetAll()
+        {
+            using (var server = new InMemoryServer())
+            {
+                // Get all products from server
+                var products = await server.SendRequest<Products>("/products", HttpMethod.Get);
+
+                // Confirm 2 items returned and check some values
+                Assert.AreEqual(2, products.Items.Count);
+                Assert.AreEqual(new Guid("8f2e9176-35ee-4f0a-ae55-83023d2db1a3"), products.Items[0].Id);
+                Assert.AreEqual("Apple iPhone 6S", products.Items[1].Name);
+            }
+        }
+    }
+}
